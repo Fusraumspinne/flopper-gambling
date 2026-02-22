3d94ea8216d2b60d6e656c04b401528e2084dcc7
Marvin <mjjaeger2002@gmail.com>
2026-02-22 18:48:24 +0100

    nerf

--- app/(games)/gatesofolympus/page.tsx
+++ app/(games)/gatesofolympus/page.tsx
@@ -32,7 +32,7 @@ const SYMBOL_WEIGHTS: Record<WeightedSymbol, number> = {
 };
 
 const MULTIPLIER_WEIGHT_NORMAL = 0.5;
-const MULTIPLIER_WEIGHT_FREE = 5;
+const MULTIPLIER_WEIGHT_FREE = 3;
 
 const SYMBOL_BASE_MULTIS: Record<BaseSymbol, number> = {
   "🌿": 0.003,
